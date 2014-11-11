3f9adf23c1b772be6c5ebb6e46e89999d0475591
Alaina <asdvalenzuela@gmail.com>
2014-11-11 14:37:38 -0800

    remove use of geojson and am now transmitting json directly to produce markers, markers are different colors depending on a condition, new set of markers is loaded every 10 seconds

--- app.py
+++ app.py
@@ -1,46 +1,17 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, Response
+import model
 import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def display_map():
-	return render_template("map.html")
+    return render_template("map.html")
 
-@app.route("/geojson")
-def geojson():
-	geojson = json.loads(open("templates/point.geojson").read())
-	return jsonify(geojson)
-
-@app.route("/test")
-def test():
-	lat_ = 1
-	long_ = 1
-	title = "Yay"
-	features = [
-    	{ "type": "Feature",
-    		"geometry": {
-    			"type": "Point",
-    			"coordinates": [lat_, long_]
-    		},
-    		"properties": {
-    		  "title": title,
-    		  "marker-color": "#333",
-    		  "marker-size" :"medium",
-    		  "marker-symbol": "building"
-    		}
-    	}
-    ]
-	new_pts = {
-    	"type": "FeatureCollection",
-    	"features": [ features ]
-    }
-
-
-	return json.dumps(new_pts)
-
-	# geojson = json.loads(open("templates/point2.geojson").read())
-	# return jsonify(geojson)
+@app.route("/coordinates")
+def return_coords():
+    coordinate_list = model.get_tweets_from_db()
+    return Response(json.dumps(coordinate_list), mimetype="application/json")
 
 if __name__ == "__main__":
-	app.run(debug = True)
+    app.run(debug = True)
